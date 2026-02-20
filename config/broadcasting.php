532926d3ec4c3e090a98a6a903332cc2935bcf60
svnfnty <jundel.caroz13@gmail.com>
2026-02-21 00:41:15 +0800

    feat: Add the default Laravel broadcasting configuration file.

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -3,30 +3,30 @@
 return [
 
     /*
-    |--------------------------------------------------------------------------
-    | Default Broadcaster
-    |--------------------------------------------------------------------------
-    |
-    | This option controls the default broadcaster that will be used by the
-    | framework when an event needs to be broadcast. You may set this to
-    | any of the connections defined in the "connections" array below.
-    |
-    | Supported: "reverb", "pusher", "ably", "redis", "log", "null"
-    |
-    */
+     |--------------------------------------------------------------------------
+     | Default Broadcaster
+     |--------------------------------------------------------------------------
+     |
+     | This option controls the default broadcaster that will be used by the
+     | framework when an event needs to be broadcast. You may set this to
+     | any of the connections defined in the "connections" array below.
+     |
+     | Supported: "reverb", "pusher", "ably", "redis", "log", "null"
+     |
+     */
 
     'default' => env('BROADCAST_CONNECTION', 'reverb'),
 
     /*
-    |--------------------------------------------------------------------------
-    | Broadcast Connections
-    |--------------------------------------------------------------------------
-    |
-    | Here you may define all of the broadcast connections that will be used
-    | to broadcast events to other systems or over WebSockets. Samples of
-    | each available type of connection are provided inside this array.
-    |
-    */
+     |--------------------------------------------------------------------------
+     | Broadcast Connections
+     |--------------------------------------------------------------------------
+     |
+     | Here you may define all of the broadcast connections that will be used
+     | to broadcast events to other systems or over WebSockets. Samples of
+     | each available type of connection are provided inside this array.
+     |
+     */
 
     'connections' => [
 
@@ -42,7 +42,7 @@ return [
                 'useTLS' => env('REVERB_SCHEME', 'http') === 'https',
             ],
             'client_options' => [
-                // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
+                'verify' => false, // Prevents timeouts when Web App talks to Reverb on Railway
             ],
         ],
 
@@ -53,7 +53,7 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
+                'host' => env('PUSHER_HOST') ?: 'api-' . env('PUSHER_APP_CLUSTER', 'mt1') . '.pusher.com',
                 'port' => env('PUSHER_PORT', 443),
                 'scheme' => env('PUSHER_SCHEME', 'https'),
                 'encrypted' => true,
