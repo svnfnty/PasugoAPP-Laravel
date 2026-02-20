6817b68e15bd705e00aea572b464adae08793df5
svnfnty <jundel.caroz13@gmail.com>
2026-02-20 21:46:48 +0800

    `Updated Android app build configuration and code to reflect changes in dependencies and settings.`

--- android/app/src/main/java/com/pasugo/app/MainActivity.java
+++ android/app/src/main/java/com/pasugo/app/MainActivity.java
@@ -1,5 +1,37 @@
 package com.pasugo.app;
 
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.BridgeWebViewClient;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = getBridge().getWebView();
+        webView.setWebViewClient(new BridgeWebViewClient(getBridge()) {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                String url = request.getUrl().toString();
+                if (url.startsWith("http:") || url.startsWith("https://")) {
+                    view.loadUrl(url);
+                    return true;
+                }
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.startsWith("http:") || url.startsWith("https://")) {
+                    view.loadUrl(url);
+                    return true;
+                }
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+        });
+    }
+}
